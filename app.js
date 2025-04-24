b6a7228b94f419437d14f30cc1dc8761f89940f0
Martín Eduardo Torres Platas <martin.torresp@uanl.edu.mx>
2025-04-24 13:59:39 -0600

    Creé unos cuantos archivos que se van a ir usando que no tienen nada aún para ir haciendo la estructura y ya está casi todo el front de las paginas que puede acceder el usuario

--- app.js
+++ app.js
@@ -2,28 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const ubicacionRouter = require('./routers/ubicacion.routes');
+const menuRouter = require('./routers/menu.routes');
+const usersRouter = require('./routers/users.routes');
+const user_opRouter = require('./routers/users_op.routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('Usuario/index');
-});
+app.use(usersRouter);
 
-app.get('/historia', (req, res) => {
-    res.render('Usuario/historia');
-});
+app.use(user_opRouter);
 
-app.get('/ubicacion', (req, res) => {
-    res.render('Usuario/ubicacion');
-});
+app.use(ubicacionRouter);
 
-app.get('/menu', (req, res) => {
-    res.render('Usuario/menu');
-});
-
+app.use(menuRouter)
 
 
 app.listen(port, () => {
