c0d271da4d589ac80a39545c3a35552f8bc8b34f
DiegoEdu19 <diegoduran7647@gmail.com>
2025-05-11 13:58:57 -0700

    fix bug

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const ubicacionRouter = require('./routers/ubicacion.routes');
-const menuRouter = require('./routers/menu.routes');
-const usersRouter = require('./routers/users.routes');
-const user_opRouter = require('./routers/gestionUsuarios.routes');
-const reportesRouter =require('./routers/gestionReservas.routes');
-const gestionHorarios = require('./routers/gestionHorarios.routes');
-const gestionDisponibilidad = require('./routers/gestionDisponibilidad.routes');
+const ubicacionRouter = require('./routes/ubicacion.routes');
+const menuRouter = require('./routes/menu.routes');
+const usersRouter = require('./routes/users.routes');
+const user_opRouter = require('./routes/gestionUsuarios.routes');
+const reportesRouter =require('./routes/gestionReservas.routes');
+const gestionHorarios = require('./routes/gestionHorarios.routes');
+const gestionDisponibilidad = require('./routes/gestionDisponibilidad.routes');
+const ubicacion = require('./routes/ubicacion.routes');
 
 // Servir archivos estáticos desde public/
 app.use(express.static(path.join(__dirname, '../public')));
@@ -27,6 +28,7 @@ app.use(menuRouter);
 app.use(reportesRouter);
 app.use(gestionHorarios);
 app.use(gestionDisponibilidad);
+app.use(ubicacion);
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${port}/`);
