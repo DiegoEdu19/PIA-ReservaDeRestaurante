fcf13950123d3355b7bb04dafca342a3075aaeb2
Martín Eduardo Torres Platas <martin.torresp@uanl.edu.mx>
2025-05-23 00:41:32 -0600

    le moví a muchas cosas del login y de admin, aún no jala.

--- bd_querys/bd.sql
+++ bd_querys/bd.sql
@@ -1,3 +1,16 @@
 INSERT INTO restaurante (nombre, direccion, ciudad, estado, codigo_postal)
-VALUES ('BigFish', 'Dalea 821, El Sabino Cerrada Residencial',
-        'Monterrey', 'Nuevo León', '64984');
+VALUES ('BigFish',
+        'Dalea 821, El Sabino Cerrada Residencial',
+        'Monterrey',
+        'Nuevo León',
+        '64984');
+
+INSERT INTO rol (descripcion)
+VALUES ('admin');
+
+INSERT INTO empleado (nombre, apellidos, correo,
+                      telefono, contrasena, id_rol,
+                      id_restaurante)
+VALUES ('martinix', 'pecesuelix',
+        'metp360@gmail.com', '8119903780',
+        'poyoyounles3243', '1', '1');
